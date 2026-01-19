95380881fb6154476f6406686e89a6bb76d36a3c
Shota Arakaki <syotaa1@gmail.com>
2026-01-20 06:20:24 +0900

    remove unneeded

--- secrets/secrets.nix
+++
@@ -1,6 +0,0 @@
-let
-  schwertleite = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQwQo4sWMxuMvTykhzzz/uFlZ2ze3kgDRhbSKhNppLc";
-in {
-  "rclone.onedrive.token.age".publicKeys = [ schwertleite ];
-  "rclone.onedrive.drive_id.age".publicKeys = [ schwertleite ];
-}
