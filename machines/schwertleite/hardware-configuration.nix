c1a530d28c6c2f8f43c526476cfa70d47568667a
Shota Arakaki <syotaa1@gmail.com>
2026-01-21 01:08:32 +0900

    fix mask

--- machines/schwertleite/hardware-configuration.nix
+++ machines/schwertleite/hardware-configuration.nix
@@ -40,7 +40,7 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/90A8-C0E5";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices = [ ];
